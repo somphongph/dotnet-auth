1b990f6ef0504241fa664eddbe783f8c4cc1c34b
Somphong Phuangmalai <somphong.ph@gmail.com>
2023-01-21 22:27:14 +0700

    add controller auth

---
+++ API/Controllers/AccountsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using Domain.Models;
+using Domain.Services.Accounts.Commands.CreateAccount;
+using Domain.Services.Accounts.Queries.GetAccountById;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("/v1/accounts")]
+public class AccountsController : ControllerBase
+{
+    private readonly ILogger<AuthController> _logger;
+    private readonly IMediator _mediator;
+
+    public AccountsController(ILogger<AuthController> logger, IMediator mediator)
+    {
+        _logger = logger;
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "admin, superuser")]
+    [ProducesResponseType(typeof(ResponseCommand<CreateAccount>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> Create(CreateAccount command)
+    {
+        var res = await _mediator.Send(command);
+
+        return Ok(res);
+    }
+
+    [HttpGet("{userId}")]
+    [Authorize]
+    [ProducesResponseType(typeof(ResponseCommand<GetAccountById>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> Get([FromRoute] GetAccountById query)
+    {
+        var res = await _mediator.Send(query);
+
+        return Ok(res);
+    }
+}
+
---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using Domain.Models;
+using Domain.Services.Auth.Commands.CreateAuthorize;
+using Domain.Services.Auth.Commands.CreateRefreshToken;
+using Domain.Services.Auth.Commands.CreateToken;
+using Domain.Services.Auth.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("/v1/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly ILogger<AuthController> _logger;
+    private readonly IMediator _mediator;
+
+    public AuthController(ILogger<AuthController> logger, IMediator mediator)
+    {
+        _logger = logger;
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+
+    [HttpPost("authorize")]
+    [ProducesResponseType(typeof(ResponseCommand<CreateAuthorizeResponse>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> Authorize(CreateAuthorize command)
+    {
+        var res = await _mediator.Send(command);
+
+        return Ok(res);
+    }
+
+    [HttpPost("token")]
+    [ProducesResponseType(typeof(ResponseCommand<TokenJwt>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> Token(CreateToken command)
+    {
+        var res = await _mediator.Send(command);
+
+        return Ok(res);
+    }
+
+    [HttpPost("refresh-token")]
+    [ProducesResponseType(typeof(ResponseCommand<TokenJwt>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> RefreshToken(CreateRefreshToken command)
+    {
+        var res = await _mediator.Send(command);
+
+        return Ok(res);
+    }
+}
+
